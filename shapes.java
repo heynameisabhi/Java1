7a39e5311be7526f399f6347efb85c71b4244ec4
heynameisabhi <169142129+heynameisabhi@users.noreply.github.com>
2024-09-17 15:10:07 +0530

    Add files via upload

---
+++ shapes.java
@@ -0,0 +1,52 @@
+package new1;
+class Shape{
+	public void draw() {
+		System.out.println("Drawing a shape");
+	}
+	public void erases()
+	{
+		System.out.println("erasing a shape");
+	}
+	
+}
+class Circle extends Shape{
+	@Override
+	public void draw()
+	{
+		System.out.println("Drawing a circle");
+	}
+	public void erase() {
+		System.out.println("Erasing a circle");
+		}
+	}
+class Triangle extends Shape{
+	@Override
+	public void draw() {
+		System.out.println("Drawing a triangle");
+		}
+	public void erase() {
+		System.out.println("Erasing a triangle");
+		}
+	}
+class Square extends Shape{
+	@Override
+	public void draw() {
+		System.out.println("Drawing a square");
+		}
+	public void erase() {
+		System.out.println("Erasing a square");
+		}
+	}
+
+public class shapes {
+	public static void main(String[] args) {
+		Shape[] shapes=new Shape[3]; //array containing Shape
+		shapes[0]=new Circle();
+		shapes[1]=new Triangle();
+		shapes[2]=new Square();
+		for(int i=0;i<shapes.length;i++) {
+			shapes[i].draw(); //polymorphic method invocation
+			shapes[i].erases();
+			System.out.println("------------");}}}
+
+
