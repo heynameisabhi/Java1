b1796590fbacfdbcbca63ec1c9cd35d3786d550a
heynameisabhi <169142129+heynameisabhi@users.noreply.github.com>
2024-11-05 20:22:08 +0530

    Add files via upload

---
+++ M7.java
@@ -0,0 +1,12 @@
+package oopj1;
+import oopj1.Tools1;
+public class M7 {
+
+	public static void main(String[] args) {
+		Tools1 t1=new Tools1();
+		System.out.println(t1.reverse("Abhishek kini"));
+		
+
+	}
+
+}
